db8307944f64dcf76eb9d8201229795e3480c4f9
MOHAMMED DANISH MUSTAFA <118317112+MOHAMMEDDANISHMUSTAFA@users.noreply.github.com>
2025-10-03 22:08:19 +0200

    Update danish_gpt.py

--- danish_gpt.py
+++ danish_gpt.py
@@ -214,4 +214,4 @@ def generate_answer(question):
                                  do_sample=False,
                                  num_beams=2)
     raw_answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    for kw in ["Generative AI", "NLP", "Big Data", "Data Science", "LangChain", "
+    for kw in ["Generative AI", "NLP", "Big Data", "Data Science", "LangChain"]
